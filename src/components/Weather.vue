0d6a73e1137187107ffd52c6935c5d58f4b848bd
Mena Nasser <menanasser234@outlook.sa>
2021-12-03 19:51:59 +0200

    Add expansion panel content in mobile screen.

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,42 +1,14 @@
 <template>
     <div>
-        <v-expansion-panels
-            id="expansion-panel"
-            v-if="$vuetify.breakpoint.mobile"
-        >
-            <v-expansion-panel>
-                <v-expansion-panel-header>
-                    <div class="d-flex justify-space-between">
-                        <div class="d-flex align-center">
-                            <div class="text-body-2 d-flex flex-column">
-                                <div class="">
-                                    {{ currentDate }}
-                                </div>
-                                <div class="grey--text">
-                                    {{ description }}
-                                </div>
-                            </div>
-                        </div>
-                        <div class="d-flex align-center">
-                            <v-img
-                                :lazy-src="iconUrl"
-                                :src="iconUrl"
-                                height="60px"
-                                width="60px"
-                            >
-                            </v-img>
-                            <div class="d-flex flex-column justify-space-between mr-3">
-                                <div class="mb-2">{{ maxTemp }}°C</div>
-                                <div class="grey--text">{{ minTemp }}°C</div>
-                            </div>
-                        </div>
-                    </div>
-                </v-expansion-panel-header>
-                <v-expansion-panel-content>
-                    something too
-                </v-expansion-panel-content>
-            </v-expansion-panel>
-        </v-expansion-panels>
+        <WeatherMobileScreen v-if="$vuetify.breakpoint.mobile"
+        :currentDate="currentDate"
+        :description="description"
+        :iconUrl="iconUrl"
+        :maxTemp="maxTemp"
+        :minTemp="minTemp"
+        :precipitation="precipitation"
+        :humidity="humidity"
+        :windSpeed="windSpeed"></WeatherMobileScreen>
         <v-container class="grey lighten-4 mt-10" v-else>
             <v-row class="ma-1 white rounded-lg main-row align-center">
                 <v-expansion-panels>
@@ -68,12 +40,13 @@ import axios from "axios";
 import WeatherDetails from "./WeatherDetails.vue";
 import WeekDaysWeather from "./WeekDaysWeather.vue";
 import roundNumberMixin from '@/mixins/roundNumberMixin'
-
+import WeatherMobileScreen from './WeatherMobileScreen.vue';
 export default {
     mixins: [roundNumberMixin],
     components: {
         WeatherDetails,
         WeekDaysWeather,
+        WeatherMobileScreen
     },
     data() {
         return {
@@ -130,6 +103,15 @@ export default {
         },
         maxTemp() {
             return this.roundNumber(this.currentDay.temp.max)
+        },
+        precipitation() {
+            return this.roundNumber(this.currentDay.pop * 100)
+        },
+        humidity() {
+            return this.currentDay.humidity
+        },
+        windSpeed() {
+            return this.roundNumber(this.currentDay.wind_speed)
         },
         iconUrl() {
             return `http://openweathermap.org/img/wn/${this.currentDay.weather[0]?.icon}@2x.png`;
--- src/components/WeatherMobileScreen.vue
+++ src/components/WeatherMobileScreen.vue
@@ -1,49 +1,74 @@
 <template>
-  <v-expansion-panels
-            id="expansion-panel"
-            v-if="$vuetify.breakpoint.mobile"
-        >
-            <v-expansion-panel>
-                <v-expansion-panel-header>
-                    <div class="d-flex justify-space-between">
-                        <div class="d-flex align-center">
-                            <div class="text-body-2 d-flex flex-column">
-                                <div class="">
-                                    {{ currentDate }}
-                                </div>
-                                <div class="grey--text">
-                                    {{ description }}
-                                </div>
+    <v-expansion-panels id="expansion-panel">
+        <v-expansion-panel>
+            <v-expansion-panel-header>
+                <div class="d-flex justify-space-between">
+                    <div class="d-flex align-center">
+                        <div class="text-body-2 d-flex flex-column">
+                            <div class="mb-1">
+                                {{ currentDate }}
                             </div>
-                        </div>
-                        <div class="d-flex align-center">
-                            <v-img
-                                :lazy-src="iconUrl"
-                                :src="iconUrl"
-                                height="60px"
-                                width="60px"
-                            >
-                            </v-img>
-                            <div class="d-flex flex-column justify-space-between mr-3">
-                                <div class="mb-2">{{ maxTemp }}°C</div>
-                                <div class="grey--text">{{ minTemp }}°C</div>
+                            <div class="grey--text">
+                                {{ description }}
                             </div>
                         </div>
                     </div>
-                </v-expansion-panel-header>
-                <v-expansion-panel-content>
-                    something too
-                </v-expansion-panel-content>
-            </v-expansion-panel>
-        </v-expansion-panels>
+                    <div class="d-flex align-center">
+                        <v-img
+                            :lazy-src="iconUrl"
+                            :src="iconUrl"
+                            height="60px"
+                            width="60px"
+                        >
+                        </v-img>
+                        <div
+                            class="d-flex flex-column justify-space-between mr-3 text-right"
+                        >
+                            <div class="mb-2">{{ maxTemp }}°C</div>
+                            <div class="grey--text">{{ minTemp }}°C</div>
+                        </div>
+                    </div>
+                </div>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+                <v-divider></v-divider>
+                <div class="d-flex justify-space-between align-center">
+                    <div class="d-flex flex-column mt-3 text-body-2">
+                        <div>
+                            <span class="grey--text mr-1">Precipitation: </span>
+                            <span>{{ precipitation }}%</span>
+                        </div>
+                        <div>
+                            <span class="grey--text mr-1">Humidity: </span>
+                            <span>{{ humidity }}%</span>
+                        </div>
+                        <div>
+                            <span class="grey--text mr-1">Wind: </span>
+                            <span>{{ windSpeed }}m/s</span>
+                        </div>
+                    </div>
+                    <div class="text-h2 mt-3">
+                        2°C
+                    </div>
+                </div>
+            </v-expansion-panel-content>
+        </v-expansion-panel>
+    </v-expansion-panels>
 </template>
 
 <script>
 export default {
-
-}
+    props: {
+        currentDate: String,
+        description: String,
+        iconUrl: String,
+        maxTemp: Number,
+        minTemp: Number,
+        precipitation: Number,
+        humidity: Number,
+        windSpeed: Number,
+    },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
